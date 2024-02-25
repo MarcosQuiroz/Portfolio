7605fb873a1b6e183f35cdb18bbb9309050844b2
Marcos Fabian Quiroz Moreno <97689381+MarcosQuiroz@users.noreply.github.com>
2024-02-25 08:56:18 -0500

    Add fade in transition to main

--- loadingscripting.js
+++ loadingscripting.js
@@ -1,31 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const loadingContent = document.querySelector('.loading-content');
+    const body = document.body;
 
     // Set the duration of the loading time in milliseconds (e.g., 5000 for 5 seconds)
-    const loadingDuration = 5000;
-
-    // Set the number of steps for scaling (adjust as needed)
-    const scaleSteps = 50;
-
-    // Calculate the interval for each scaling step
-    const scaleInterval = loadingDuration / scaleSteps;
+    const loadingDuration = 1000;
 
     // Function to handle the fade-out effect and transition to the main page
     function fadeOut() {
-        let currentScale = 1.0;
-        const scaleIncrement = 0.02; // Adjust the increment as needed
-
-        // Use setInterval to gradually increase the scale and change opacity
-        const scaleIntervalId = setInterval(function () {
-            currentScale += scaleIncrement;
-            loadingContent.style.transform = `scale(${currentScale})`;
-            loadingContent.style.opacity -= 1 / scaleSteps; // Decrease opacity
-        }, scaleInterval);
+        loadingContent.style.opacity = 0;
+        body.style.scale = 10; // Apply the scale transformation
 
         // Wait for the transition to complete before redirecting
         setTimeout(function () {
             window.location.href = 'main.html'; // Replace 'main.html' with your main page
-        }, loadingDuration);
+        }, 1000); // Adjust the time according to your transition duration
     }
 
     // Set a timeout to trigger the fade-out effect after the loading duration
@@ -45,7 +33,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     function isMobileDevice() {
-        return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        // Assuming you have a proper check for mobile devices
+        return window.innerWidth <= 600; // Adjust the threshold as needed
     }
 
     const skipText = document.querySelector('.skip-text');
--- scripting.js
+++ scripting.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const container = document.querySelector('.container');
+    const body = document.body;
+
+    // Set the duration of the loading time in milliseconds (e.g., 5000 for 5 seconds)
+    const loadingDuration = 1000;
+
+    // Function to handle the fade-out effect and transition to the main page
+    function fadeOut() {
+        container.style.opacity = 1;
+        body.style.scale = 1; // Apply the scale transformation
+    }
+
+    setTimeout(fadeOut, loadingDuration);
+
     var linkButtons = document.querySelectorAll('.LinkButton');
   
     linkButtons.forEach(function (button) {
