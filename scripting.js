140d9038e052ed686b60cbca3a64dcde24a61f46
Marcos Fabian Quiroz Moreno <97689381+MarcosQuiroz@users.noreply.github.com>
2024-02-25 23:29:20 -0500

    Modifying transitions

--- scripting.js
+++ scripting.js
@@ -1,37 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  const bar = document.querySelector('.bar');
-
-  function fadeIn() {
-        bar.style.opacity = 1;
-        bar.style.transform = 'translateX(500px)';
+  // Function to handle the fade-out effect and transition to the main page
+  function fadeInBar() {
+      const bar = document.querySelector('.bar');
+      bar.style.opacity = 1;
+      bar.style.transform = 'translateX(0px)';
   }
 
-    setTimeout(fadeIn, 0.3);
+  function fadeInlinksAndName() {
+      const name = document.querySelector('.name');
+      const links = document.querySelector('.linksBar');
 
+      name.style.opacity = 1;
+      links.style.opacity = 1;
+  }
 
-    var linkButtons = document.querySelectorAll('.LinkButton');
-  
-    linkButtons.forEach(function (button) {
-      button.addEventListener('touchstart', function () {
-        button.classList.add('active'); // Add a class on touch
-      });
-  
-      button.addEventListener('touchend', function () {
-        button.classList.remove('active'); // Remove the class when touch ends
-      });
+  fadeInlinksAndName();
+  setTimeout(function() { fadeInBar(); }, 1000);  
+
+  var linkButtons = document.querySelectorAll('.LinkButton');
+
+  linkButtons.forEach(function (button) {
+    button.addEventListener('touchstart', function () {
+      button.classList.add('active'); // Add a class on touch
+    });
+
+    button.addEventListener('touchend', function () {
+      button.classList.remove('active'); // Remove the class when touch ends
     });
   });
+});
 
-  document.getElementById('resumeLink').addEventListener('click', function (event) {
-    event.preventDefault();
+document.getElementById('resumeLink').addEventListener('click', function (event) {
+  event.preventDefault();
 
-    const isMobile = window.matchMedia('(max-width: 600px)').matches;
+  const isMobile = window.matchMedia('(max-width: 600px)').matches;
 
-    if (isMobile) {
-        const downloadLink = document.getElementById('mobileDownloadLink');
-        downloadLink.click();
-    } else {
-        window.location.href = 'resume.html';
-    }
+  if (isMobile) {
+      const downloadLink = document.getElementById('mobileDownloadLink');
+      downloadLink.click();
+  } else {
+      window.location.href = 'resume.html';
+  }
 });
