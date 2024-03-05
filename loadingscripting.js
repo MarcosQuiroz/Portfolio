57c8476a0ce653ee0c181f1ce12c80aea22567fc
Marcos Fabian Quiroz Moreno <97689381+MarcosQuiroz@users.noreply.github.com>
2024-03-05 17:45:47 -0500

    Add jump animation

--- loadingscripting.js
+++ loadingscripting.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const spritesheet = document.querySelector('.character-spritesheet');
-  const text = document.querySelector('p');
-  const skipText = document.querySelector('.skip-text');
+document.addEventListener("DOMContentLoaded", function () {
+  const spritesheet = document.querySelector(".character-spritesheet");
+  const text = document.querySelector("p");
+  const skipText = document.querySelector(".skip-text");
 
   const loadingDuration = 60000;
 
   function resetStyles() {
-      spritesheet.style.opacity = 1;
-      spritesheet.style.transform = 'translateX(0)';
-      text.style.opacity = 1;
-      text.style.transform = 'translateX(0)';
-    }
+    spritesheet.style.opacity = 1;
+    spritesheet.style.transform = "translateX(0)";
+    text.style.opacity = 1;
+    text.style.transform = "translateX(0)";
+  }
 
   function fadeOut() {
     spritesheet.style.opacity = 0;
-    spritesheet.style.transform = 'translateX(500px)';
+    spritesheet.style.transform = "translateX(500px)";
 
     text.style.opacity = 0;
-    text.style.transform = 'translateX(500px)';
+    text.style.transform = "translateX(500px)";
 
     setTimeout(function () {
-      window.location.href = 'main.html';
+      window.location.href = "main.html";
     }, 1000);
 
     setTimeout(function () {
@@ -30,13 +30,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
   setTimeout(fadeOut, loadingDuration);
 
-  document.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
       fadeOut();
     }
   });
 
-  skipText.addEventListener('touchstart', function (event) {
+  skipText.addEventListener("touchstart", function (event) {
     if (isMobileDevice()) {
       event.stopPropagation();
       fadeOut();
@@ -53,56 +53,60 @@ document.addEventListener('DOMContentLoaded', function () {
     skipText.textContent = "Press 'ESC' to skip";
   }
 
-  const loadingScreen = document.querySelector('.loading-content');
+  const loadingScreen = document.querySelector(".loading-content");
 
-  const spriteWidth = 80;
+  const spriteWidth = 160;
   const totalFrames = 10;
   const animationSpeed = 50;
 
   let currentFrame = 0;
   let verticalPosition = 0;
   let verticalVelocity = 0;
-  const gravity = 0.91;
-  const jumpStrength = -12;
+  const gravity = 2;
+  const jumpStrength = -26;
 
   let isGrounded = false;
+  /*spritesheet.style.backgroundImage = `url('https://raw.githubusercontent.com/MarcosQuiroz/Portfolio/main/images/Player_Run.png')`;*/
+  /*spritesheet.style.backgroundImage = `url('https://raw.githubusercontent.com/MarcosQuiroz/Portfolio/main/images/Player_Jump.png')`;*/
+  spritesheet.style.backgroundImage = `url('images/Player_Run.png')`;
 
   function animateSpritesheet() {
     const position = -currentFrame * spriteWidth;
-    spritesheet.style.backgroundImage = `url('https://raw.githubusercontent.com/MarcosQuiroz/Portfolio/main/images/Player_Run.png')`;
-      spritesheet.style.backgroundPosition = `${position}px`;
-      spritesheet.style.transform = `translateY(${verticalPosition}px)`;
+    spritesheet.style.backgroundPosition = `${position}px`;
+    spritesheet.style.transform = `translateY(${verticalPosition}px)`;
 
-  currentFrame = (currentFrame + 1) % totalFrames;
-    
-  verticalVelocity += gravity;
-  verticalPosition += verticalVelocity;
-    
-  const playerGroundPosition = verticalPosition ;
-    
-  if (playerGroundPosition > 0) {
+    currentFrame = (currentFrame + 1) % totalFrames;
+
+    verticalVelocity += gravity;
+    verticalPosition += verticalVelocity;
+
+    const playerGroundPosition = verticalPosition;
+
+    if (playerGroundPosition > 0) {
+      spritesheet.style.backgroundImage = `url('images/Player_Run.png')`;
       verticalPosition = 0;
       verticalVelocity = 0;
       isGrounded = true;
+    } else {
+      isGrounded = false;
     }
-    else
-      {
-        isGrounded = false;
-      }
   }
-  
+
   function jump() {
     if (isGrounded) {
-      verticalVelocity = jumpStrength; 
-    }}
+      verticalVelocity = jumpStrength;
+      currentFrame = 0
+      spritesheet.style.backgroundImage = `url('images/Player_Jump.png')`;
+    }
+  }
 
-    document.addEventListener('click', jump);
-    document.addEventListener('touchstart', jump);
+  document.addEventListener("click", jump);
+  document.addEventListener("touchstart", jump);
 
   const animationInterval = setInterval(animateSpritesheet, animationSpeed);
 
   setTimeout(function () {
     clearInterval(animationInterval);
-    loadingScreen.style.display = 'none';
+    loadingScreen.style.display = "none";
   }, 60000);
 });
