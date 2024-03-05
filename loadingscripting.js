2e8215b3458212a4e58c13ccc83bb5a0cd9d52c6
Marcos Fabian Quiroz Moreno <97689381+MarcosQuiroz@users.noreply.github.com>
2024-03-04 21:33:04 -0500

    Trying adding animation

--- loadingscripting.js
+++ loadingscripting.js
@@ -1,6 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const loadingScreen = document.getElementById('.loading-content');
-    const spritesheet = document.getElementById('.character-spritesheet');
+    const spritesheet = document.querySelector('.character-spritesheet');
+    const text = document.querySelector('p');
+    const skipText = document.querySelector('.skip-text');
+
+    const loadingDuration = 5000;
+
+    function fadeOut() {
+        spritesheet.style.opacity = 0;
+        spritesheet.style.transform = 'translateX(500px)';
+
+        text.style.opacity = 0;
+        text.style.transform = 'translateX(500px)';
+
+        setTimeout(function () {
+            window.location.href = 'main.html'; 
+        }, 1000); 
+    }
+
+    setTimeout(fadeOut, loadingDuration);
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            fadeOut();
+        }
+    });
+
+    skipText.addEventListener('touchstart', function (event) {
+        if (isMobileDevice()) {
+            event.stopPropagation();
+            fadeOut();
+        }
+    });
+
+    function isMobileDevice() {
+        return window.innerWidth <= 600;
+    }
+
+    if (isMobileDevice()) {
+        skipText.textContent = "Touch HERE to skip";
+    } else {
+        skipText.textContent = "Press 'ESC' to skip";
+    }
+
+   const loadingScreen = document.querySelector('.loading-content');
   
     const spriteWidth = 80;
     const totalFrames = 10;
@@ -24,32 +66,5 @@ document.addEventListener('DOMContentLoaded', function () {
       clearInterval(animationInterval);
       loadingScreen.style.display = 'none';
     }, 60000);
-
-    const skipText = document.querySelector('.skip-text');
-
-    // Allow users to skip the loading by pressing 'ESC'
-    document.addEventListener('keydown', function (event) {
-        if (event.key === 'Escape') {
-            fadeOut();
-        }
-    });
-
-    skipText.addEventListener('touchstart', function (event) {
-        if (isMobileDevice()) {
-            event.stopPropagation();
-            fadeOut();
-        }
-    });
-
-    function isMobileDevice() {
-        // Assuming you have a proper check for mobile devices
-        return window.innerWidth <= 600; // Adjust the threshold as needed
-    }
-
-    if (isMobileDevice()) {
-        skipText.textContent = "Touch HERE to skip";
-    } else {
-        skipText.textContent = "Press 'ESC' to skip";
-    }
 
 });
