b0a30c540dc41f60e72894a5bc55d56711232aa5
Marcos Fabian Quiroz Moreno <97689381+MarcosQuiroz@users.noreply.github.com>
2024-06-14 18:45:04 -0400

    Adding back image slide

--- gamescripting.js
+++ gamescripting.js
@@ -13,4 +13,16 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
-});
+
+     const images = document.querySelectorAll(".background-image");
+      let currentIndex = 0;
+
+      function showNextImage() {
+        images[currentIndex].classList.remove("active");
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add("active");
+      }
+
+      setInterval(showNextImage, 5000);
+      showNextImage();
+    });
