89b3565e36a24a4795142d16bab3ed722a008e2c
Marcos Fabian Quiroz Moreno <97689381+MarcosQuiroz@users.noreply.github.com>
2024-06-14 19:15:15 -0400

    trying fixing image slide

--- gamescripting.js
+++ gamescripting.js
@@ -14,15 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-     const images = document.querySelectorAll(".background-image");
-      let currentIndex = 0;
+  const images = document.querySelectorAll(".background-image");
+  let currentIndex = 0;
 
-      function showNextImage() {
-        images[currentIndex].classList.remove("active");
-        currentIndex = (currentIndex + 1) % images.length;
-        images[currentIndex].classList.add("active");
-      }
+  function showNextImage() {
+    images[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add("active");
+  }
 
-      setInterval(showNextImage, 5000);
-      showNextImage();
-    });
+  setInterval(showNextImage, 5000);
+  showNextImage();
+});
